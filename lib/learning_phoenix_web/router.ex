c1385df0f91dae01c4899a9200fee7595ca1bb96
William Hergès <william@herges.fr>
2025-08-14 20:49:11 +0200

    feat(user): use gen auth for users

--- lib/learning_phoenix_web/router.ex
+++ lib/learning_phoenix_web/router.ex
@@ -1,6 +1,8 @@
 defmodule LearningPhoenixWeb.Router do
   use LearningPhoenixWeb, :router
 
+  import LearningPhoenixWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule LearningPhoenixWeb.Router do
     plug :put_root_layout, html: {LearningPhoenixWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_scope_for_user
   end
 
   pipeline :api do
@@ -18,7 +21,6 @@ defmodule LearningPhoenixWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
-    resources "/users", UserController
   end
 
   # Other scopes may use custom stacks.
@@ -41,5 +43,33 @@ defmodule LearningPhoenixWeb.Router do
       live_dashboard "/dashboard", metrics: LearningPhoenixWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
+  end
+
+  ## Authentication routes
+
+  scope "/", LearningPhoenixWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{LearningPhoenixWeb.UserAuth, :require_authenticated}] do
+      live "/users/settings", UserLive.Settings, :edit
+      live "/users/settings/confirm-email/:token", UserLive.Settings, :confirm_email
+    end
+
+    post "/users/update-password", UserSessionController, :update_password
+  end
+
+  scope "/", LearningPhoenixWeb do
+    pipe_through [:browser]
+
+    live_session :current_user,
+      on_mount: [{LearningPhoenixWeb.UserAuth, :mount_current_scope}] do
+      live "/users/register", UserLive.Registration, :new
+      live "/users/log-in", UserLive.Login, :new
+      live "/users/log-in/:token", UserLive.Confirmation, :new
+    end
+
+    post "/users/log-in", UserSessionController, :create
+    delete "/users/log-out", UserSessionController, :delete
   end
 end
